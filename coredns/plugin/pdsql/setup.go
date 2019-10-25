85dfa05b46865a1713c69305c3742174a90958f8
long <150389523@qq.com>
2019-10-25 20:18:05 +0800

    update caddyserver
    update caddyserver

--- coredns/plugin/pdsql/setup.go
+++ coredns/plugin/pdsql/setup.go
@@ -6,7 +6,7 @@ import (
 	"github.com/coredns/coredns/core/dnsserver"
 	"github.com/coredns/coredns/plugin"
 	"github.com/jinzhu/gorm"
-	"github.com/mholt/caddy"
+	"github.com/caddyserver/caddy"
 	pdnsmodel "github.com/wenerme/wps/pdns/model"
 )
 
